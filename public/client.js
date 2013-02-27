8d595a2c74f97c7a68256bfe42cd426c1c75a8f2
andypaxo <andy@andypaxo.net>
2013-02-26 19:13:37 -0700

    Client is now also using common code

--- public/client.js
+++ public/client.js
@@ -9,9 +9,7 @@ $(function () {
 	canvas.mozImageSmoothing = false;
 	canvas.webkitImageSmoothingEnabled = false;
 	canvas.imageSmoothing = false;
-	var time_offset = 0;
-	var players = {};
-	var time_step = 1;
+	var world = new World();
 
 	var player_images = new Image();
 	player_images.src="/lofi_char.png";
@@ -33,8 +31,6 @@ $(function () {
 		var posX = $(this).offset().left, posY = $(this).offset().top;
 		var x = e.pageX - posX, y = e.pageY - posY;
 		socket.emit('move-player', {x:x,y:y});
-		//players[my_identifier].x = x;
-		//players[my_identifier].y = y;
 	});
 
 	socket.on('connect', function() {
@@ -44,8 +40,8 @@ $(function () {
 	socket.on('allocate-id', function(data) {
 		my_identifier = data.id;
 		// Not currently accounting for latency
-		time_offset = data.now - now();
-		console.log('Time offset = ' + time_offset);
+		world.time_offset = data.now - world.now();
+		console.log('Time offset = ' + world.time_offset);
 	});
 
 	socket.on('chat-message', function(data) {
@@ -53,8 +49,8 @@ $(function () {
 	});
 
 	socket.on('status', function(data) {
-		players = data.players_online;
-		$('#status-users').text(Object.keys(players).length);
+		world.players = data.players_online;
+		$('#status-users').text(Object.keys(world.players).length);
 	});
 
 	function log_chat(name, message_text) {
@@ -66,35 +62,18 @@ $(function () {
 		return pick_random_from(names) + '-' + pick_random_from(names);
 	}
 
-	function moveTowardDestination(player) {
-		var time = now();
-		var destination = player.destination;
-		if(destination.eta > time  + time_step) {
-			var x_displacement = time_step * (destination.x - player.x) / (destination.eta - time);
-			var y_displacement = time_step * (destination.y - player.y) / (destination.eta - time);
-			player.x += x_displacement;
-			player.y += y_displacement;
-		} else {
-			player.x = destination.x;
-			player.y = destination.y;
-		}
-	}
-
 	// Drawing
 
 	var drawFrame = function() {
-		time_step = now() - last_time;
-		last_time = now();
-
-		movePlayers();
+		world.movePlayers();
 
 		canvas.fillStyle = '#221100';
 		canvas.fillRect(0, 0, 400, 400);
 		
 		canvas.fillStyle = '#22ff22';
 		var strip_width = player_images.width / 8;
-		for (player_id in players) {
-			var player = players[player_id];
+		for (player_id in world.players) {
+			var player = world.players[player_id];
 			canvas.fillText(player.name, player.x, player.y);
 			canvas.drawImage(player_images,
 				8 * (player.image % strip_width), 8 * Math.floor(player.image / strip_width), 8, 8,
@@ -105,38 +84,12 @@ $(function () {
 			window.requestAnimationFrame(drawFrame);
 	};
 
-	var last_time = now();
 	if (window.requestAnimationFrame)
 		window.requestAnimationFrame(drawFrame);
 	else
 		window.setInterval(drawFrame, 50);
 
-
-	function movePlayers() {
-		for(var id in players)
-			if (players[id].destination)
-				moveTowardDestination(players[id]);
-	}
-
-	function moveTowardDestination(player) {
-		var time = now() + time_offset;
-		var destination = player.destination;
-		if(destination.eta > time  + time_step) {
-			var x_displacement = time_step * (destination.x - player.x) / (destination.eta - time);
-			var y_displacement = time_step * (destination.y - player.y) / (destination.eta - time);
-			player.x += x_displacement;
-			player.y += y_displacement;
-		} else {
-			player.x = destination.x;
-			player.y = destination.y;
-		}
-	}
-
 	// Utility
-
-	function now() { 
-		return new Date().getTime() / 1000;
-	}
 
 	function pick_random_from(array) {
 		return array[Math.floor(Math.random() * array.length)];
--- public/hollowroot_world.js
+++ public/hollowroot_world.js
@@ -1,22 +1,24 @@
-exports.World = World;
+if (typeof exports !== 'undefined')
+	exports.World = World;
 
 function World()
 {
 	this.players = {};
 	this.last_integration = this.now();
+	this.time_offset = 0;
 }
 
 World.prototype.movePlayers = function() {
 	for(var id in this.players)
 		if (this.players[id].destination)
 			this.moveTowardDestination(this.players[id]);
+	this.last_integration = this.now();
 }
 
 World.prototype.moveTowardDestination = function(player) {
 	var time = this.now();
 	var destination = player.destination;
-	var time_step = this.now() - this.last_integration;
-	this.last_integration = this.now();
+	var time_step = time - this.last_integration;
 	if(destination.eta > time  + time_step) {
 		var x_displacement = time_step * (destination.x - player.x) / (destination.eta - time);
 		var y_displacement = time_step * (destination.y - player.y) / (destination.eta - time);
@@ -31,7 +33,7 @@ World.prototype.moveTowardDestination = function(player) {
 // Utility
 
 World.prototype.now = function() {
-	return new Date().getTime() /  1000;
+	return new Date().getTime() /  1000 + this.time_offset;
 }
 
 World.prototype.distance = function(x1, y1, x2, y2) {
